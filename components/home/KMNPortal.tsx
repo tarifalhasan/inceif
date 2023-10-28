06af759bb31f032da1533c3183797b711616ea5a
Tarif Al Hasan <92237522+tarifalhasan@users.noreply.github.com>
2023-10-28 14:47:49 +0600

    update

--- components/home/KMNPortal.tsx
+++ components/home/KMNPortal.tsx
@@ -15,7 +15,7 @@ const KMNPortal = () => {
               sizes="(min-width: 1540px) 760px, (min-width: 1280px) 632px, (min-width: 1040px) 504px, (min-width: 780px) 752px, (min-width: 640px) 624px, (min-width: 520px) 459px, calc(87.5vw + 22px)"
             />
           </div>
-          <div className="space-y-5 w-1/2 lg:w-full">
+          <div className="space-y-5 w-full lg:w-1/2">
             <h2 className="heading">KMC Portal</h2>
             <p className="description max-w-[538px]">
               Discover and get access to Islamic and Conventional Finance
